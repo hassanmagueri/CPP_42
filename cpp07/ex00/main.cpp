24a5d4ef7dfdea6cd9af3ff7f2b003559ddf2a01
HaSaN <90562289+hassanmagueri@users.noreply.github.com>
2025-04-25 17:49:17 +0100

    ex00 cpp 07

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <typeinfo>
+#include "whatever.hpp"
+
+
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
+
+template <typename t1> void swap(t1 &a, t1 &b)
+{
+    t1 c = a;
+    a = b;
+    b = c;
+}
+
+template <typename t> t min(t a, t b)
+{
+    return a < b ? a : b;
+}
+
+template <typename t> t max(t a, t b)
+{
+    return a > b ? a : b;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+template <typename t1> void swap(t1 &a, t1 &b);
+template <typename t> t min(t a, t b);
+template <typename t> t max(t a, t b);
+
+
